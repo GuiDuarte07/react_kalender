4b048263dd5173b5481e6a872047885311935462
Guilherme Duarte <guilduarte07@gmail.com>
2023-07-06 13:19:47 -0300

    configuring mui with tailwind and news packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body id='__next' className={inter.className}>{children}</body>
     </html>
   )
 }
