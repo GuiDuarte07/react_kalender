23b28dea3e82b7cf30cb9d0583ab5757325a2f3d
Guilherme Duarte <guilduarte07@gmail.com>
2023-07-06 11:44:16 -0300

    Mui install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client"
 import { Calendar } from '@/components/Calendar'
+import MiniDrawer from '@/components/Drawer';
+import { Container } from '@mui/material'
+import MuiDrawer from '@mui/material/Drawer';
+
 
 export default function Home() {
   return (
-    <main className='min-w-full min-h-screen'>
+    <main className='min-w-full min-h-screen flex'>
+      <Container>
+      <MiniDrawer/>
+      </Container>
+      <Container maxWidth="xl">
       <section className='w-full h-[500px]'><Calendar/></section>
+      </Container>
     </main>
+    
   )
 }
+
+{/* <main className='min-w-full min-h-screen'>
+      <
+    </main> */}
