5635b2c9b1d18339dda896ccb9db0a8b6c4e07c8
Guilherme Duarte <guilduarte07@gmail.com>
2023-07-07 14:39:13 -0300

    deleteEvent reducer logic

--- src/hooks/EventHandleReducer.tsx
+++ src/hooks/EventHandleReducer.tsx
@@ -1,5 +1,4 @@
 import { IKalendEvent } from '@/interfaces/Kalend';
-import { useReducer } from 'react';
 
 export enum EventReducerActionTypes {
   NewEvent,
@@ -10,25 +9,32 @@ export enum EventReducerActionTypes {
 type Action =
   | {type: EventReducerActionTypes.NewEvent, newEvent: {endAt: string, startAt: string, summary: string} }
   | {type: EventReducerActionTypes.EditEvent, editEvent: {id: number, endAt: string, startAt: string, summary: string}}
+  | {type: EventReducerActionTypes.DeleteEvent, id: number}
 
 function eventHandleReducer(state: IKalendEvent[], action: Action) {
-  const newState = structuredClone(state);
+  const newState = structuredClone(state)
 
   switch (action.type) {
     case EventReducerActionTypes.NewEvent:
       newState.push({id: newState.length+1, color: 'red', endAt: action.newEvent.endAt, startAt: action.newEvent.startAt, summary: action.newEvent.summary})
-      break;
+      break
     
     case EventReducerActionTypes.EditEvent:
       const index = newState.findIndex((event) => event.id === action.editEvent?.id)
-      console.log(index, action.editEvent?.id)
-      if (index === -1) break;
+      if (index === -1) break
+
       newState[index].startAt = action.editEvent.startAt
       newState[index].endAt = action.editEvent.endAt
       newState[index].summary = action.editEvent.summary
       break;
+    case EventReducerActionTypes.DeleteEvent:
+      const idx = newState.findIndex((event) => event.id === action.id)
+      if (idx === -1) break
+
+      newState.splice(idx, 1)
+      break
   }
   return newState
 }
 
-export default eventHandleReducer;
+export default eventHandleReducer
